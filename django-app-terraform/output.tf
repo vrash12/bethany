a10c93709e8ce1bd3f9d611a24f2dde8484da733
unknown <vthres@gmail.com>
2024-11-07 20:12:08 +0800

    dd

--- django-app-terraform/output.tf
+++
@@ -1,9 +0,0 @@
-output "instance_ip" {
-  description = "The public IP of the web server"
-  value       = "13.54.94.104"
-}
-
-output "instance_id" {
-  description = "The ID of the EC2 instance"
-  value       = "i-00c435b1c1bd79d4e"
-}
